8f5e210adde5e1b9a7cd56feb1e434bdc395be3d
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-11-17 12:19:38 +0200

    Add files via upload

---
+++ C#/1703A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++) 
+                list.Add(Console.ReadLine());
+            for (int i = 0;i < n; i++)
+                if (list[i].ToLower() == "yes")
+                    Console.WriteLine("YES");
+            else
+                    Console.WriteLine("NO");
+
+
+        }
+    }
+}
