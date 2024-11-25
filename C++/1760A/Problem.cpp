63eb9a25ce4375462cfd9d33caf6147ef9680c28
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-11-25 12:53:31 +0200

    Update Problem.cpp

--- C++/1760A/Problem.cpp
+++ C++/1760A/Problem.cpp
@@ -11,8 +11,7 @@ short getMedium(short arr[3])
 	if ((arr[1] > arr[0] && arr[1] < arr[2]) || (arr[1] < arr[0] && arr[1] > arr[2]))
 		return arr[1]; 
 
-	if ((arr[2] > arr[0] && arr[2] < arr[1]) || (arr[2] < arr[1] && arr[2] > arr[0]))
-		return arr[2];
+	return arr[2];
 }
 
 int main()
