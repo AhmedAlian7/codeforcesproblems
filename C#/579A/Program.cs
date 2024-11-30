b27afa1d2290eedce207b6ce13e103ec7962c170
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-11-30 14:05:24 +0200

    Add files via upload

---
+++ C#/579A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _579A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = Convert.ToUInt32(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+            int result = 0;
+            foreach (var bit in binary)
+            {
+                if (bit == '1')
+                    ++result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
