d5ab7e92f086004955c047bf8b45a839fda257b1
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-11-30 15:09:24 +0200

    Add files via upload

---
+++ C#/758A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _758A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = Convert.ToUInt32(Console.ReadLine());
+            var a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int max = a.Max();
+            long sum = a.Sum(x => max - x);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
