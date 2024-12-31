4cefc07aa8ac299a3d2f674111ec2957c43e3dc7
AhmedAlian7 <158756069+AhmedAlian7@users.noreply.github.com>
2024-12-31 20:12:30 +0200

    Add files via upload

---
+++ C++/732A/Problem.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+#define ll long long
+
+
+int minShovels(int k, int r) // 15 2
+{
+	int i = 0;
+	while (++i)
+	{
+		int last_digit = (k * i) % 10;
+		if (last_digit == r || last_digit == 0)
+			return i;
+	}
+	return -1;
+}
+int main()
+{
+	
+	int k, r; cin >> k >> r;
+	cout << minShovels(k, r) << endl;
+	return 0;
+}
